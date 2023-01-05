1c093d558d37f28cab920877db47b80d76dae437
sagi <ogoritoo@gmail.com>
2023-01-05 17:18:16 +0200

    close #2

--- simple/script.js
+++ simple/script.js
@@ -1,12 +1,32 @@
-let crt;
+let crt = "clock";
+let id;
 
 function giveMeSite(IDfun) {
-  let id = IDfun; //incoming of what to show
+  switch (
+    IDfun //translation from income to results
+  ) {
+    case 1:
+      id = "clock";
+      break;
+    case 2:
+      id = "todo";
+      break;
+    case 3:
+      id = "rps";
+      break;
+    case 4:
+      id = "vowel";
+      break;
+    case 5:
+      id = "note";
+      break;
+  }
+  var mainFrameOne = document.getElementById(crt);
+  crt = id; //for the next time
+  var mainFrameTwo = document.getElementById(crt);
 
-  let element = document.getElementById(crt); //hide the current
-  let hidden = element.getAttribute("hidden");
-  element.setAttribute("hidden", "hidden");
-  crt = id; //for the next time hopefully
+  mainFrameOne.style.display = "none"; //get rid of the last desplay
 
-  element.removeAttribute("hidden"); //show
+  mainFrameTwo.style.display =
+    mainFrameTwo.style.display == "none" ? "block" : "none"; //showing the income click
 }
